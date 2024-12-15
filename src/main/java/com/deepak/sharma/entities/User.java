d560b0ac715cfc16161ebfee72e747479f7f81eb
Deepak Sharma <dsharma2828@gmail.com>
2024-12-15 13:33:44 +0530

    user entity added

---
+++ src/main/java/com/deepak/sharma/entities/User.java
@@ -0,0 +1,12 @@
+package com.deepak.sharma.entities;
+
+import java.util.List;
+
+public class User {
+    private String userId;
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+
+}
