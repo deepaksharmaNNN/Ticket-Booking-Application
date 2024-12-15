176ff83379ea5ed4516b2a0fd54063a4ede25fb5
Deepak Sharma <dsharma2828@gmail.com>
2024-12-15 18:27:43 +0530

    Droppig this Project

--- src/main/java/com/deepak/sharma/services/TrainService.java
+++ src/main/java/com/deepak/sharma/services/TrainService.java
@@ -7,5 +7,15 @@ import java.util.stream.Collectors;
 
 public class TrainService {
 
+    private List<Train> trainList;
+    public void getTrainDetails() {
+        System.out.println("Train details");
+    }
 
+    public List<Train> searchTrains(String source, String destination) {
+        System.out.println("Searching trains from " + source + " to " + destination);
+        return trainList.stream().filter(train -> {
+            return train.getStations().contains(source) && train.getStations().contains(destination);
+        }).collect(Collectors.toList());
+    }
 }
