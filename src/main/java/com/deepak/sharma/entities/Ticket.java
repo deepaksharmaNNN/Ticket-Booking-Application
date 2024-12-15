c025f98c00fd59de4e737814f35c25df87364e3d
Deepak Sharma <dsharma2828@gmail.com>
2024-12-15 13:37:58 +0530

    train entity added

---
+++ src/main/java/com/deepak/sharma/entities/Ticket.java
@@ -0,0 +1,13 @@
+package com.deepak.sharma.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Date dateOfJourney;
+    private Train train;
+
+}
